5c49057a29c2e4457ce372d4958a253e79dae7d7
roger067 <rosestolato.rogerio@gmail.com>
2023-08-16 09:58:11 -0300

    carousel works

--- src/app/hotel-card/hotel-card.component.ts
+++ src/app/hotel-card/hotel-card.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-hotel-card',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './hotel-card.component.html',
   styleUrls: ['./hotel-card.component.scss'],
 })
